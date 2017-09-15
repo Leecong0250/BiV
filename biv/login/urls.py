71b85658781330c8c0b5ea253bd3b4ef9495e277
licong <licong0250@163.com>
2017-09-15 14:04:48 +0800

    first

---
+++ biv/login/urls.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+from django.conf.urls import patterns, url
+from login.views import Login
+
+urlpatterns = patterns(
+    '',
+     url(r'^$',Login.as_view(), name='login'),
+)
